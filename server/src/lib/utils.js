beaf6836d38128f96a97f619936bee23453642a5
Ananducn <anandu.cn112@gmail.com>
2025-05-01 23:09:19 +0530

    cookies setting changed

--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -5,13 +5,20 @@ export const generateToken = (userId, res) => {
     expiresIn: "7d",
   });
 
+  // res.cookie("jwt", token, {
+  //   maxAge: 7 * 24 * 60 * 60 * 1000, // this is 7 days in milliseconds
+  //   httpOnly: true, // this is to prevent the cookie from being accessed by the client/attackers
+  //   sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+  //   // this is to prevent the cookie from being sent to other domains
+  //   secure: process.env.NODE_ENV === "production", // Enforce in production only
+  //   // cookie to be sent only over https
+  // });
+
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // this is 7 days in milliseconds
-    httpOnly: true, // this is to prevent the cookie from being accessed by the client/attackers
-    sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
-    // this is to prevent the cookie from being sent to other domains
-    secure: process.env.NODE_ENV === "production", // Enforce in production only
-    // cookie to be sent only over https
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
   });
 
   return token;
