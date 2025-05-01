b25478e7ed003b769415c1b38f33fccf6f7d66a0
Ananducn <anandu.cn112@gmail.com>
2025-05-01 22:49:07 +0530

    cors link added

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://blabber-chat-backend.onrender.com",
+  baseURL: "https://blabber-chat-backend.onrender.com/api",
   withCredentials: true,
 });
